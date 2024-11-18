52ed83f425301d7655c853bc4c76f9e5cd738040
surya <suryait263@gmail.com>
2024-11-18 20:12:19 +0530

    Initial commit

---
+++ app/src/main/java/com/cometchat/kotlinsampleapp/AppConstants.kt
@@ -0,0 +1,11 @@
+package com.cometchat.kotlinsampleapp
+
+object AppConstants {
+
+    const val APP_ID = "xxxxxxxxxxx"
+
+    const val AUTH_KEY = "xxxxxxxxxxxxxxxxxxxx"
+
+    const val REGION = "xx"
+
+}
